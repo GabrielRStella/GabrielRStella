6610fc59dbbbbbdb03ca909540c1762d8ab97d21
Gabriel <XyPhoGR@gmail.com>
2017-08-19 08:09:20 -0500

    command-line arguments

--- Node/app/src/server.js
+++ Node/app/src/server.js
@@ -4,18 +4,29 @@ var express = require('express');
 
 //TODO: may move handlebars to routes
 var handlebars = require('handlebars');
-var hbhelpers = require('./hbhelpers.js');
+var hbhelpers = require('./hbhelpers');
 
 var routes = require('./routes');
-
-//for reading files async
-//var fs = require('fs');
-
-//console.log(hbtemplates);
 
 //
 
 hbhelpers(handlebars);
+
+//parse command-line args
+
+var args = process.argv.slice(2);
+
+var options = {
+  port: 80,
+};
+
+for(var idx in args) {
+  var arg = args[idx];
+  var split = arg.split("=");
+  var key = split[0];
+  var value = split.slice(1).join("=");
+  options[key] = JSON.parse(value);
+}
 
 //begin server
 
@@ -25,13 +36,13 @@ app.use('/static', express.static('../public'));
 
 routes(app, express, handlebars);
 
-app.listen(3000, function () {
+app.listen(options.port, function () {
   //just for fun
   var source = "Server started on port {{port}}.{{#if todo}} TODO: {{todo}}{{/if}}";
 
   var template = handlebars.compile(source);
   var data = {
-    port: 3000,
+    port: options.port,
     todo: "command line ports"
   };
   var result = template(data);
