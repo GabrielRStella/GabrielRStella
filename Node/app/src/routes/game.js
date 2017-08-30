d43722736d2c34a18f46818eaa54dc7aba46f643
Gabriel <XyPhoGR@gmail.com>
2017-08-30 12:53:15 -0500

    more cleanup

--- Node/app/src/routes/game.js
+++ Node/app/src/routes/game.js
@@ -6,34 +6,18 @@ var page = require('../util/page');
 var fs = require('fs');
 var handlebars = require('handlebars');
 
+var loadGames = require('../util/loadgames');
+
 module.exports = function(req, res) {
 
-    var path = './games/' + req.params.game;
-
-    fs.stat(path, function(err, stats) {
-      var content = null;
-      if(err) {
-        content = templates.content.game_invalid({
-          palette: {
-            background: palette.foreground
-          },
-        });
-      } else {
-        var gameData = require('.' + path + '/game.json');
-        gameData.template_after = !gameData.template_before;
-        if(fs.existsSync(path + '/game.hb')) {
-          var template = handlebars.compile(fs.readFileSync(path + '/game.hb', 'utf8'));
-          gameData.template = template({
-            //nothing yet
-          });
-        }
-        content = templates.content.game({
-          palette: {
-            background: palette.foreground
-          },
-          game: gameData
-        });
-      }
-      page(content, res.send.bind(res));
+    loadGames.loadGame(req.params.game, function(game) {
+      page(templates.content.game({
+          palette: palette,
+          game: game
+        }), res.send.bind(res));
+    }, function() {
+      page(templates.content.game_invalid({
+        palette: palette,
+      }), res.send.bind(res));
     });
 }
