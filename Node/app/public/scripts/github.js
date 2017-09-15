b64a3afdb35d37736fd62da0de7357c75a5e8673
Gabriel <XyPhoGR@gmail.com>
2017-09-15 17:04:26 -0500

    date formatting, layout, and reset button

--- Node/app/public/scripts/github.js
+++ Node/app/public/scripts/github.js
@@ -1,16 +1,25 @@
+var days = [
+"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+];
+var months = [
+"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
+];
+
 class Commit extends React.Component {
   render() {
     var commit = this.props.commit;
     var url = commit.html_url;
     commit = commit.commit;
     var author = commit.author.name;
-    var date = commit.author.date;
+    var date = new Date(commit.author.date);
+    var dateString = days[date.getDay()] + ", " + date.getDate() + " " + months[date.getMonth()] + " " + date.getFullYear();
     var msg = commit.message;
+
 
     return React.createElement('tr', {},
       React.createElement('td', {}, author),
       React.createElement('td', {}, msg),
-      React.createElement('td', {}, date));
+      React.createElement('td', {}, dateString));
   }
 }
 
@@ -18,8 +27,11 @@ class Commits extends React.Component {
   render() {
     return React.createElement('div', {},
       React.createElement('style', {}, `
+table {
+  border: solid 1px black;
+}
 tr:nth-child(even) {
-    background-color: #dddddd;
+  background-color: #dddddd;
 }
       `),
       React.createElement('table', {},
@@ -36,8 +48,21 @@ tr:nth-child(even) {
 
 class Error extends React.Component {
   render() {
-    return React.createElement('p', {style: {color: "red"}}, "Error: " + this.props.error,
-      React.createElement('br', {}));
+    return React.createElement('div', {style: {border: "solid 1px red"}},
+      React.createElement('p', {style: {color: "red"}}, "Error: " + this.props.error,
+        React.createElement('br', {})
+      )
+    );
+  }
+}
+
+class Placeholder extends React.Component {
+  render() {
+    return React.createElement('div', {style: {border: "solid 1px black"}},
+      React.createElement('p', {}, this.props.message,
+        React.createElement('br', {})
+      )
+    );
   }
 }
 
@@ -53,6 +78,7 @@ class Options extends React.Component {
     this.onUserChange = this.onUserChange.bind(this);
     this.onRepoChange = this.onRepoChange.bind(this);
     this.onSearch = this.onSearch.bind(this);
+    this.onClear = this.onClear.bind(this);
   }
 
   onUserChange(event) {
@@ -67,6 +93,14 @@ class Options extends React.Component {
     this.props.onSearch(this.state.user, this.state.repo);
   }
 
+  onClear() {
+    this.setState({
+      user: "GabrielRStella",
+      repo: "GabrielRStella"
+    });
+    this.props.onSearch(null, null);
+  }
+
 //TODO: make the repo selection a dropdown, loaded from the user
   render() {
     //heheheheheh
@@ -78,9 +112,12 @@ class Options extends React.Component {
         React.createElement('input', {type: "text", id: "field_user", value: this.state.user, onChange: this.onUserChange}),
         React.createElement('label', {"for": "field_user"}, "User")
       ),
-      React.createElement('div', {className: "input-field col s6"},
+      React.createElement('div', {className: "input-field col s5"},
         React.createElement('input', {type: "text", id: "field_repo", value: this.state.repo, onChange: this.onRepoChange}),
         React.createElement('label', {"for": "field_repo"}, "Repository")
+      ),
+      React.createElement('div', {className: "btn-flat col s1", onClick: this.onClear},
+        "Reset"
       ),
       React.createElement('div', {className: "btn col s1 black", onClick: this.onSearch},
         React.createElement('i', {className: "material-icons"}, icon)
@@ -102,11 +139,18 @@ class Page extends React.Component {
   }
 
   load(user, repo) {
+    if(!user || !repo) {
+      thisFake.setState({commits: null, error: null});
+      return;
+    }
+
     var url = "https://api.github.com/repos/" + user + "/" + repo + "/commits";
-    this.setState({commits: null, error: null});
     var thisFake = this;
+
     $.ajax(url, {
       dataType: "json"
+    }).always(function(){
+      thisFake.setState({commits: null, error: null});
     }).done(function(data, status, obj) {
       thisFake.setState({commits: data, error: null});
     }).fail(function(obj, status, err) {
@@ -125,15 +169,13 @@ class Page extends React.Component {
     } else if(this.state.error) {
       body = React.createElement(Error, {error: this.state.error});
     } else {
-      body = React.createElement('p', {}, "Hi!");
+      body = React.createElement(Placeholder, {message: "Hi! Please enter a github user and repository name, then press the button to load commits."});
     }
 
     return React.createElement('div', {className: "container"}, 
       React.createElement(Options, {onSearch: this.load}),
-      React.createElement('div', {style: {border: "solid 1px black"}},
-        React.createElement('div', {style: {alignContent: "center", textAlign: "center"}},
-          body
-        )
+      React.createElement('div', {style: {alignContent: "center", textAlign: "center"}},
+        body
       )
     );
   }
