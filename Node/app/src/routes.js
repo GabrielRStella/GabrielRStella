5af33b0e05305c7adfadb32403b678b5fe6e8e6c
Gabriel <XyPhoGR@gmail.com>
2017-12-19 22:35:25 -0600

    react stuff

--- Node/app/src/routes.js
+++ Node/app/src/routes.js
@@ -10,7 +10,11 @@ module.exports = function(app, express) {
 
   app.get('/', route_react('index'));
 
-  app.get('/github', route_react('github'));
+  //i feel like this could be dangerous...but i like it ;)
+  app.get('/:script', route_react());
+
+  //no longer necessary thanks to the above
+  //app.get('/github', route_react('github'));
 
   router_api(app, express);
 
--- Node/app/src/routes/react_route.js
+++ Node/app/src/routes/react_route.js
@@ -2,6 +2,6 @@ var templates = require('../util/templates.js');
 
 module.exports = function(script) {
   return function(req, res) {
-    res.send(templates.page(script));
+    res.send(templates.page(script || req.params.script));
   };
 }
