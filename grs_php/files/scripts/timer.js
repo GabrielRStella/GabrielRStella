0b51f346d838a47a45052eefb503d05648967de6
Gabriel <gabrielrstella@gmail.com>
2022-04-16 00:44:52 -0500

    fixed up formatting for mobile+desktop

--- grs_php/files/scripts/timer.js
+++ grs_php/files/scripts/timer.js
@@ -27,25 +27,59 @@ class Message extends React.Component {
 
 //TODO fix this for mobile (use two rows; I think valign-wrapper breaks it)
 class Options extends React.Component {
+  createContent(args) {
+  }
+  
   render() {
-    return React.createElement('div', {className: "row valign-wrapper"},
-      React.createElement('div', {className: "input-field col m4 s12"},
-        React.createElement('input', {type: "time", id: "field_begin", value: this.props.timeBegin, onChange: this.props.onTimeBeginChange}),
-        React.createElement('label', {"for": "field_begin"}, "Begin")
-      ),
-      React.createElement('div', {className: "input-field col m4 s12"},
-        React.createElement('input', {type: "time", id: "field_end", value: this.props.timeEnd, onChange: this.props.onTimeEndChange}),
-        React.createElement('label', {"for": "field_end"}, "End")
-      ),
-      React.createElement('div', {className: "input-field col m2 s6"},
-        React.createElement('input', {type: "number", step:"1", id: "field_fontsize", value: this.props.fontSize, onChange: this.props.onFontSizeChange}),
-        React.createElement('label', {"for": "field_fontsize"}, "Font size")
-      ),
-        React.createElement('label', {className: "input-field col m2 s6"},
-			React.createElement('input', {type: "checkbox", className: "filled-in", id: "field_showpercent", checked: this.props.showPercent, onChange: this.props.onShowPercentChange}),
-			React.createElement('span', {}, "Show Percentage")
-		)
-    );
+    //materializecss uses the following width calculations to determine screen size category:
+    //large: >= 993px
+    //small: <= 600px
+    if(window.innerWidth <= 600) {
+      //small
+    
+      return React.createElement('div', {},
+        React.createElement('div', {className: "row"},
+          React.createElement('div', {className: "input-field col l3 s6"},
+            React.createElement('input', {type: "time", id: "field_begin", value: this.props.timeBegin, onChange: this.props.onTimeBeginChange}),
+            React.createElement('label', {"for": "field_begin"}, "Begin")
+          ),
+          React.createElement('div', {className: "input-field col l3 s6"},
+            React.createElement('input', {type: "time", id: "field_end", value: this.props.timeEnd, onChange: this.props.onTimeEndChange}),
+            React.createElement('label', {"for": "field_end"}, "End")
+          ),
+        ),
+        React.createElement('div', {className: "row valign-wrapper"},
+          React.createElement('div', {className: "input-field col l2 s4"},
+            React.createElement('input', {type: "number", step:"1", id: "field_fontsize", value: this.props.fontSize, onChange: this.props.onFontSizeChange}),
+            React.createElement('label', {"for": "field_fontsize"}, "Font size")
+          ),
+          React.createElement('label', {className: "input-field col l4 s8"},
+            React.createElement('input', {type: "checkbox", className: "filled-in", id: "field_showpercent", checked: this.props.showPercent, onChange: this.props.onShowPercentChange}),
+            React.createElement('span', {}, "Show Percentage")
+          ),
+        )
+      );
+    } else {
+      //medium or large
+      return React.createElement('div', {className: "row valign-wrapper"},
+        React.createElement('div', {className: "input-field col l3 s6"},
+          React.createElement('input', {type: "time", id: "field_begin", value: this.props.timeBegin, onChange: this.props.onTimeBeginChange}),
+          React.createElement('label', {"for": "field_begin"}, "Begin")
+        ),
+        React.createElement('div', {className: "input-field col l3 s6"},
+          React.createElement('input', {type: "time", id: "field_end", value: this.props.timeEnd, onChange: this.props.onTimeEndChange}),
+          React.createElement('label', {"for": "field_end"}, "End")
+        ),
+        React.createElement('div', {className: "input-field col l2 s4"},
+          React.createElement('input', {type: "number", step:"1", id: "field_fontsize", value: this.props.fontSize, onChange: this.props.onFontSizeChange}),
+          React.createElement('label', {"for": "field_fontsize"}, "Font size")
+        ),
+        React.createElement('label', {className: "input-field col l4 s8"},
+          React.createElement('input', {type: "checkbox", className: "filled-in", id: "field_showpercent", checked: this.props.showPercent, onChange: this.props.onShowPercentChange}),
+          React.createElement('span', {}, "Show Percentage")
+        )
+      );
+    }
   }
 }
 
