e1a696dbf62e5ae53f8800e725688a81c2e229b5
Gabriel <gabrielrstella@gmail.com>
2025-05-20 00:52:23 -0500

    note

--- grs_php/files/scripts/cspace-logic.js
+++ grs_php/files/scripts/cspace-logic.js
@@ -1,2 +1,3 @@
 //translating point robot, translating circle robot, 2-dof arm with fixed base, arm that rotates on a prismatic joint
 //tpoint, tcircle, arm, slider
+//canvas ids: canvas-robot-x and canvas-cspace-x
